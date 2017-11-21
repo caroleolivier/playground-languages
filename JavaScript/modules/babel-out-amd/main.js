6a09aabe7d850c23980a77fdb616c121599f610d
carole.olivier <carole.olivier@gmail.com>
2017-11-21 14:52:29 +0000

    Remove files that should not have been committed

--- JavaScript/modules/babel-out-amd/main.js
+++
@@ -1,5 +0,0 @@
-define(['./simple'], function (_simple) {
-  'use strict';
-
-  console.log(_simple.str);
-});
--- JavaScript/modules/babel-out/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var _simple = require('./simple');
-
-console.log(_simple.str);
